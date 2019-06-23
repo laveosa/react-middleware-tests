79705e188adef5179582bb0a57e7fa050b84b32a
Nikolay Grygoryev <nikolay@barnyardcreative.com>
2019-06-23 18:03:07 +0300

    update - Student form layout

--- src/elements/forms/studentForm/StudentForm.jsx
+++ src/elements/forms/studentForm/StudentForm.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./styles.scss";
 
 class StudentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gender: "1"
+    };
+  }
   componentWillMount() {
     console.log(this.props);
   }
@@ -46,6 +52,10 @@ class StudentForm extends React.Component {
                   type="radio"
                   name="gender"
                   ref="gender"
+                  checked
+                  onChange={event => {
+                    this.setState({ gender: event.target.value });
+                  }}
                   value="1"
                 />
                 <label htmlFor="Male" className="form-check-label">
@@ -59,6 +69,9 @@ class StudentForm extends React.Component {
                   type="radio"
                   name="gender"
                   ref="gender"
+                  onChange={event => {
+                    this.setState({ gender: event.target.value });
+                  }}
                   value="2"
                 />
                 <label htmlFor="Female" className="form-check-label">
@@ -66,6 +79,15 @@ class StudentForm extends React.Component {
                 </label>
               </div>
             </div>
+          </div>
+          <div className="form-group">
+            <label className="badge badge-secondary">email:</label>
+            <input
+              className="form-control"
+              type="email"
+              placeholder="enter email..."
+              ref="email"
+            />
           </div>
           <div className="form-group">
             <label className="badge badge-secondary">facult:</label>
@@ -86,13 +108,22 @@ class StudentForm extends React.Component {
             />
           </div>
           <div className="button-area d-flex align-items-center justify-content-end">
-            <button className="btn btn-primary m-1 w-25 text-uppercase">
+            <button
+              className="btn btn-primary m-1 w-25 text-uppercase"
+              onClick={this.generateHandler}
+            >
               generate
             </button>
-            <button className="btn btn-success m-1 w-25 text-uppercase">
+            <button
+              className="btn btn-success m-1 w-25 text-uppercase"
+              onClick={this.addHandler}
+            >
               add
             </button>
-            <button className="btn btn-secondary m-1 w-25 text-uppercase">
+            <button
+              className="btn btn-secondary m-1 w-25 text-uppercase"
+              onClick={this.clearHandler}
+            >
               clear
             </button>
           </div>
@@ -100,6 +131,38 @@ class StudentForm extends React.Component {
       </div>
     );
   }
+  generateHandler = () => {
+    this.refs.name.value = "Nik";
+    this.refs.familyName.value = "Grygoryev";
+    this.refs.age.value = "37";
+    this.refs.gender.value = "1";
+    this.setState({ gender: "1" });
+    this.refs.email.value = "laveosa@yahoo.com";
+    this.refs.facult.value = "IT";
+    this.refs.course.value = "5";
+  };
+  addHandler = () => {
+    const newStudent = {
+      name: this.refs.name.value,
+      familyName: this.refs.familyName.value,
+      age: this.refs.age.value,
+      gender: this.state.gender,
+      email: this.refs.email.value,
+      facult: this.refs.facult.value,
+      course: this.refs.course.value
+    };
+
+    this.props.addStudent(newStudent);
+  };
+  clearHandler = () => {
+    this.refs.name.value = null;
+    this.refs.familyName.value = null;
+    this.refs.age.value = null;
+    this.refs.gender.value = null;
+    this.refs.email.value = null;
+    this.refs.facult.value = null;
+    this.refs.course.value = null;
+  };
 }
 
 export default StudentForm;
