853195c1235674dacd3acd82411c3191fc0c2ece
Nikolay Grygoryev <nikolay@barnyardcreative.com>
2019-07-07 19:46:25 +0300

    update - logic in reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,15 +1,19 @@
 import * as actions from "./actions";
 
 const innerState = {
-  counter: 0
+  count: 0
 };
 
 export default (state = innerState, action) => {
   switch (action.type) {
     case actions.UP_COUNT:
-      return state;
+      return Object.assign({}, state, {
+        count: state.count + action.payload
+      });
     case actions.DOWN_COUNT:
-      return state;
+      return Object.assign({}, state, {
+        count: state.count - action.payload
+      });
     default:
       return state;
   }
